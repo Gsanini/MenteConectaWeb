de6285a0cfe46ef13af4dfc1a134a02e70c986e4
Gabriel Sanini <sanini448@gmail.com>
2024-11-13 08:50:11 -0300

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MenteConectaWeb/",
   server: {
     port: 3002,
     open: true, // Abre o navegador automaticamente
